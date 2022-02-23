2b508a5873da4da1c1fd5020b31272db4ae6756d
Wenjun Zhou <mic12130@gmail.com>
2022-02-23 13:44:49 +0800

    Added tests for op data converter

---
+++ EurocatTests/common/OpDataConverterTest.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+
+#include "common/OpDataConverter.h"
+
+namespace Eurocat::Common
+{
+	TEST(OpDataConverterTest, ConvertsEmptyStringFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat(""), "");
+	}
+
+	TEST(OpDataConverterTest, ConvertsSingleSlashFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("/"), "");
+	}
+
+	TEST(OpDataConverterTest, ConvertsStringWithSlashFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("/AAA"), "AAA");
+	}
+
+	TEST(OpDataConverterTest, ConvertsStringWithoutSlashFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("AAA"), "AAA");
+	}
+
+	TEST(OpDataConverterTest, MakesUpperWhenConvertFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("ab-cd"), "AB-CD");
+	}
+
+	TEST(OpDataConverterTest, CanClipLongStringFromEs)
+	{
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("123123123123"), "1231231231");
+	}
+
+	TEST(OpDataConverterTest, CanClipLongStringWithSlashFromEs)
+	{
+		// Slash shall be ignored in this test case
+		EXPECT_STREQ(OpDataConverter::FromEsFormat("/123123123123"), "1231231231");
+	}
+
+	TEST(OpDataConverterTest, ConvertsEmptyOpDataToEs)
+	{
+		EXPECT_STREQ(OpDataConverter::ToEsFormat(""), "");
+	}
+
+	TEST(OpDataConverterTest, ConvertsNonEmptyOpDataToEs)
+	{
+		EXPECT_STREQ(OpDataConverter::ToEsFormat("2333"), "/2333");
+	}
+}
